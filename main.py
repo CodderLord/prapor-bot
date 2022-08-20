818de7c9899117a23903a1f45fd6e3328601efee
CodderLord <Verevkinleha09092000>
2022-08-20 18:09:58 +0200

    0.1

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 ﻿import discord
-import requests
-from bs4 import BeautifulSoup
 import re
 from discord.ext import commands
 from config import settings
 from lists_conf import possible_hello, people_helpers, USER_AGENT, url, url_gallery, url_gallery_end
 from random import randint
 import urllib.parse
+from req import get_soup
+from bs4 import BeautifulSoup
 
 
 intents = discord.Intents.all()
@@ -39,20 +39,20 @@ async def on_message(message):
 	if result_bot_help is not None:
 		await message.channel.send("Сейчас поищу в бумагах, может найду что нибудь.")
 		result_message = str(message.content).replace(f"Прапор помоги с квестом ", '')
-		request = requests.get(url + urllib.parse.quote(result_message), headers=USER_AGENT)
-		request_gallery = requests.get(url_gallery + urllib.parse.quote(result_message) + url_gallery_end, headers=USER_AGENT)
+		request = get_soup((url + urllib.parse.quote(result_message)))
+		request_gallery = get_soup((url_gallery + urllib.parse.quote(result_message) + url_gallery_end))
 		try:
 			result_url_gallery = BeautifulSoup(request_gallery.text, "html.parser")
 			result_url_gallery = result_url_gallery.find(class_="unified-search__result__header").find("a").get("href")
 		except AttributeError:
 			await message.channel.send("К сожалению ничего не смог найти.")
 			return
-		result_search_url_gallery = requests.get(result_url_gallery, headers=USER_AGENT)
+		result_search_url_gallery = get_soup(result_url_gallery)
 		result_search_gallery = BeautifulSoup(result_search_url_gallery.text, "html.parser")
 		result_search_gallery = result_search_gallery.find(class_="gallery mw-gallery-packed")
 		result_search_table = BeautifulSoup(request.text, "html.parser")
 		result_table_href = result_search_table.find(class_="block block-system clearfix").find("a").get("href")
-		request_end_link = requests.get(result_table_href, headers=USER_AGENT)
+		request_end_link = get_soup(result_table_href)
 		result_end_link = BeautifulSoup(request_end_link.text, "html.parser")
 		result = result_end_link.find(class_="inside panels-flexible-region-inside panels-flexible-region-tankpanel-center-inside panels-flexible-region-inside-last").text
 		table_info = result.replace('\n\n\n\n', '\n').strip()
---
+++ req.py
@@ -0,0 +1,14 @@
+import requests
+from lists_conf import USER_AGENT
+import time
+
+
+def get_soup(urls):
+	try:
+		req = requests.get(urls, headers=USER_AGENT)
+		return req
+	except Exception as err:
+		print(f"{err=}")
+		time.sleep(3)
+		
+		
