5a65a952f1f73ef53ccbe13b8323efc5283707af
Inviking <86830902+CodderLord@users.noreply.github.com>
2024-10-31 18:16:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'MTAwMDM2NTQ1ODEzNTk3ODAwNQ.GF0Kx9.6grzfdF0StpnEXyIVhaKtSxFlTaRvxXjui83ac',
+    'token': ,
     'bot': 'Прапор',
     'id': 1000365458135978005,
     'prefix': '/'
